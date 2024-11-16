c0fa6e7ad5a72cffb69f70ba5edddd4b0dea8e2e
Caiki <caiki2003@gmail.com>
2024-11-16 14:37:47 -0300

    vai la

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,23 @@
+'use client'
+import Image from "next/image";
+
+export default function Navbar() {
+
+    const ClickHandler = (local: string) => {
+
+    }
+
+    return (
+        <header className="w-screen bg-cinza_escuro flex flex-row items-center justify-evenly py-6">
+            <Image />
+            <div className="flex flex-row justify-between w-[40vw]">
+                <button onClick={()=>ClickHandler("home")} className="text-cinza_claro hover:text-white">Home</button>
+                <button onClick={()=>ClickHandler("home")} className="text-cinza_claro hover:text-white">Serviços</button>
+                <button onClick={()=>ClickHandler("home")} className="text-cinza_claro hover:text-white">Sobre</button>
+                <button onClick={()=>ClickHandler("home")} className="text-cinza_claro hover:text-white">Skills</button>
+                <button onClick={()=>ClickHandler("home")} className="text-cinza_claro hover:text-white">Portfolio</button>
+                <button onClick={()=>ClickHandler("home")} className="text-cinza_claro hover:text-white">Contato</button>
+            </div>
+        </header>
+    )
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        cinza_escuro: "#212336",
+        cinza_claro: "#b7b7b7",
+        
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
