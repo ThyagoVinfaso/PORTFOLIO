ea4e4adb8482b8284050b50804437a7d87813402
ThyagoVinfaso <thyagovinfaso@gmail.com>
2024-11-16 15:14:14 -0300

    abrobra

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Contato from "@/components/Contato";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
       <Navbar />
+      <Contato />
     </main>
   );
 }
---
+++ src/components/Contato.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+export default function Contato(){
+
+return();
+
+}
