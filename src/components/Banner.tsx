3befd1ee8284c654a4598168bd6656205c6a9774
ThyagoVinfaso <thyagovinfaso@gmail.com>
2024-11-18 22:59:24 -0300

    Footer

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -5,9 +5,12 @@ export default function Banner() {
     return (
         <div className="w-screen h-[40vh]">
 
-            <div className="flex-col w-[40vw] h-full ml-[10vw] ">
-                <p className="text-3xl">Olá, eu sou um desenvolvedor Front-End</p>
-                <p className="mt-[2vh] text-cinza_claro">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium</p>
+            <div className="flex-col w-[36vw] h-full ml-[10vw] ">
+                <p className="text-6xl">Olá, eu sou um desenvolvedor <span className="text-vermelho">Front-End</span></p>
+                <div className="w-[20vw]">
+                <p className="mt-[2vh] text-cinza_claro">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium Sed ut perspiciatis unde omnis iste natus error</p>
+                </div>
+                
                 <div className="flex h-[6vh] w-[14vw] items-center justify-center bg-vermelho mt-[2vh] rounded-sm">
                     <p className="">Github</p>
 
