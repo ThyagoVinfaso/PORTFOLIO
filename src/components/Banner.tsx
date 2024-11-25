81810d011f0cbca93db8c559379a38da9c7c62e0
Caiki <caiki2003@gmail.com>
2024-11-25 13:07:22 -0300

    arrumado

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,15 +3,15 @@
 export default function Banner() {
 
     return (
-        <div className="w-screen h-[40vh]">
+        <div className="w-screen mb-[20vh]">
 
-            <div className="flex-col w-[36vw] h-full ml-[10vw] ">
+            <div className="flex-col desktop:w-[36vw] mobile:w-[80vw] desktop:ml-[10vw] mobile:ml-[5vw]">
                 <p className="text-6xl">Olá, eu sou um desenvolvedor <span className="text-vermelho">Front-End</span></p>
-                <div className="w-[20vw]">
+                <div className="desktop:w-[20vw]">
                 <p className="mt-[2vh] text-cinza_claro">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium Sed ut perspiciatis unde omnis iste natus error</p>
                 </div>
                 
-                <div className="flex h-[6vh] w-[14vw] items-center justify-center bg-vermelho mt-[2vh] rounded-sm">
+                <div className="flex h-[6vh] desktop:w-[14vw] mobile:w-[50vw] items-center justify-center bg-vermelho mt-[2vh] rounded-sm">
                     <p className="">Github</p>
 
                 </div>
--- src/components/Contato.tsx
+++ src/components/Contato.tsx
@@ -8,9 +8,9 @@ export default function Contato() {
         <div className="w-screen flex flex-col px-[10vw] mt-32">
             <label className="text-vermelho font-bold text-6xl pb-16">Contato</label>
             <div className="flex flex-col desktop:w-[50%] mobile:w-full gap-12">
-                <TextField className="bg-azul_cinzento_meio_claro" slotProps={{ input: { style: { color: "#b7b7b7", fontSize: 32 } }, inputLabel: { style: { color: "#b7b7b7", fontSize: 32 } } }} variant="filled" label="Email" />
-                <TextField className="bg-azul_cinzento_meio_claro" slotProps={{ input: { style: { color: "#b7b7b7", fontSize: 32 } }, inputLabel: { style: { color: "#b7b7b7", fontSize: 32 } } }} variant="filled" label="Mensagem" />
-                <button className="bg-vermelho px-8 py-4 text-2xl after:content-['_►'] w-[50%] ml-[50%]">Enviar</button>
+                <TextField className="bg-azul_cinzento_meio_claro text-2xl" slotProps={{ input: { style: { color: "#b7b7b7" } }, inputLabel: { style: { color: "#b7b7b7" } } }} variant="filled" label="Email" />
+                <TextField className="bg-azul_cinzento_meio_claro text-2xl" slotProps={{ input: { style: { color: "#b7b7b7" } }, inputLabel: { style: { color: "#b7b7b7" } } }} variant="filled" label="Mensagem" />
+                <button className="bg-vermelho px-8 py-4 text-base after:content-['_►'] w-[50%] ml-[50%]">Enviar</button>
             </div>
         </div>
     );
