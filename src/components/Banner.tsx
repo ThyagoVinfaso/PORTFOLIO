43e4cd52554574efbdf902a34bebd7cfb6d4bef4
ThyagoVinfaso <thyagovinfaso@gmail.com>
2024-11-27 13:49:41 -0300

    NoLorem2

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -8,7 +8,7 @@ export default function Banner() {
             <div className="flex-col desktop:w-[36vw] mobile:w-[80vw] desktop:ml-[10vw] mobile:ml-[5vw]">
                 <p className="text-6xl">Olá, eu sou um desenvolvedor <span className="text-vermelho">Front-End</span></p>
                 <div className="desktop:w-[20vw]">
-                <p className="mt-[2vh] text-cinza_claro">Meu nome é Thyago, sou desenvolvedor focado principalmente no Front-end e atualmente graduando do 7º período de engenharia de Software da UTFPR.</p>
+                <p className="mt-[2vh] text-cinza_claro">Meu nome é Thyago, sou desenvolvedor focado principalmente em Front-end e com forte interesse em QA, atualmente graduando do 7º período de engenharia de Software da UTFPR.</p>
                 </div>
                 
                 <button className="flex h-[6vh] desktop:w-[14vw] mobile:w-[50vw] items-center justify-center bg-vermelho mt-[2vh] rounded-sm hover:animate-pulse">
